296751cc92acbbf48dd6c4826947ef4de79228ba
Niyaz Khasanov <khasanchik85@gmail.com>
2024-08-18 23:19:36 +0500

    Файл view будет отвечать за отображение информации о фильме

---
+++ view/view.py
@@ -0,0 +1,13 @@
+class MovieView:
+    def __init__(self, controller):
+        self.controller = controller
+
+    def show_information(self):
+        information = self.controller.get_information()
+        print("Информация о фильме:")
+        for key, value in information.items():
+            print(f"{key}: {value}")
+
+    def add_actor(self, name, role):
+        self.controller.add_actor(name, role)
+        print(f"Актер {name} с ролью '{role}' добавлен.")
